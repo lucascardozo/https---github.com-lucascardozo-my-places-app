4269ace38d28268dceadae5f0ef830aba7b30ea2
lucascardozo <lucas_lrrc@hotmail.com>
2024-08-31 17:26:14 -0300

    Backend

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Import routes
 const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
-app.use(placesRoutes);
+app.use("/api/places",placesRoutes);
+
+app.use((error,req,res,next) => {
+    if(res.headerSent){
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message: error.message || "An Unknown error occured!"});
+});
 
 app.listen(5000);
--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -1,14 +1,65 @@
 const express = require('express');
+const HttpError = require("../models/http-error");
 
 const router = express.Router(); 
 
-router.get("/",(req,res,next)=>{
+const DUMMY_PLACES = [
+    {
+        id:'p1',
+        title:'Calgary',
+        description:'Calgary, uma cidade cosmopolita em Alberta com vários arranha-céus, deve seu rápido crescimento à condição de centro da indústria de petróleo do Canadá. No entanto, ela ainda está mergulhada na cultura western, que lhe valeu o apelido de "Cidade da vaca", evidente no Calgary Stampede, um grande festival de rodeio realizado em julho e que teve origem nas exposições agropecuárias organizadas no passado. ',
+        imageUrl:'https://www.pictureperfectcleaning.ca/wp-content/uploads/2020/08/Calgary-aerial-pan-from-N.jpg',
+        address:'Alberta',
+        location:{
+            lat:51.0271596,
+            lng:-114.4174685
+        },
+        creator:'12'
+    },
+    {
+        id:'p2',
+        title:'Edmonton',
+        description:'Calgary, uma cidade cosmopolita em Alberta com vários arranha-céus, deve seu rápido crescimento à condição de centro da indústria de petróleo do Canadá. No entanto, ela ainda está mergulhada na cultura western, que lhe valeu o apelido de "Cidade da vaca", evidente no Calgary Stampede, um grande festival de rodeio realizado em julho e que teve origem nas exposições agropecuárias organizadas no passado. ',
+        imageUrl:'https://www.pictureperfectcleaning.ca/wp-content/uploads/2020/08/Calgary-aerial-pan-from-N.jpg',
+        address:'Alberta',
+        location:{
+            lat:51.0271596,
+            lng:-114.4174685
+        },
+        creator:'12'
+    }
+];
 
-    console.log("GET IN PLACES");
+router.get("/:placeId",(req,res,next)=>{
 
-    res.json({
-        message:"its work!"
+    const placeId = req.params.placeId;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId
     });
+
+    if(!place){
+        throw new HttpError('Could not find a place for de provide id.',404);
+    }  
+
+    res.json({place});
+
+});
+
+router.get("/user/:userId",(req,res,next)=>{
+
+    const userId = req.params.userId;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId
+    });
+
+    if(!place){
+        return next(new HttpError('Could not find a place for de provide user id.',404)); 
+    }  
+
+    res.json({place});
+
 });
 
 module.exports = router;
